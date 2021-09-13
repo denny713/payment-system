9ca6309ea95b4b881afa93a9eef37847bf6522b4
Denny Afrizal <denny.afrizal713@gmail.com>
2021-09-13 18:29:52 +0700

    Add Some Configuration

---
+++ src/main/java/com/payment/config/TilesConfig.java
@@ -0,0 +1,26 @@
+package com.payment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class TilesConfig {
+
+    @Bean
+    public UrlBasedViewResolver viewResolver() {
+        UrlBasedViewResolver view = new UrlBasedViewResolver();
+        view.setViewClass(TilesView.class);
+        return view;
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfig() {
+        TilesConfigurer tiles = new TilesConfigurer();
+        tiles.setDefinitions(new String[]{"WEB-INF/context/tiles.xml"});
+        tiles.setCheckRefresh(true);
+        return tiles;
+    }
+}
